f0720d3f94c0006cd7c3763c31d7b62be670aa8f
saberali1789 <78509035+saberali1789@users.noreply.github.com>
2023-02-18 21:18:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const textarea =document.getElementById('textarea');
 const totalCounter = document.getElementById('total-counter');
 const remainingCounter = document.getElementById('remaining-counter');
+
 updateCounter();
+
 textarea.addEventListener('keyup', () => {
   updateCounter();
 })
+
 
 function updateCounter(){
   totalCounter.innerText = textarea.value.length;
